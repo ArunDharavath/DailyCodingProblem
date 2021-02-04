e4d30455f46e936375d5857f71fd7f7c5217699d
Arun Dharavath <53053571+ArunDharavath@users.noreply.github.com>
2021-02-04 10:02:11 +0530

    1st problem

--- prob1.py
+++ prob1.py
@@ -1,7 +1,12 @@
+"""
+Reading elements into the list.
+"""
+
 my_list = map(int, input().split())
 sum = int(input())
 count = 0
 
+#NESTED FOR LOOP to determine if any two nums in the list give the sum.
 for i in my_list:
   for j in my_list:
     if i+j == sum:
